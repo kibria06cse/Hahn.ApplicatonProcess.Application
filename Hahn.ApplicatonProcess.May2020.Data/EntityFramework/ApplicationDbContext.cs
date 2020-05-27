82c716adf0fb02318193fe6ee763b3ca8a36ea98
azad_aplectrum <azad@aplectrum.com>
2020-05-27 16:56:56 +0600

    DbContexts added

---
+++ Hahn.ApplicatonProcess.May2020.Data/EntityFramework/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Hahn.ApplicatonProcess.May2020.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hahn.ApplicatonProcess.May2020.Data.EntityFramework
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Applicant> Movie { get; set; }
+    }
+}
--- Hahn.ApplicatonProcess.May2020.Web/Startup.cs
+++ Hahn.ApplicatonProcess.May2020.Web/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hahn.ApplicatonProcess.May2020.Data.EntityFramework;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,10 @@ namespace Hahn.ApplicatonProcess.May2020.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("ApplicationDbContext")));
+
 
 
             // Register the Swagger generator, defining 1 or more Swagger documents

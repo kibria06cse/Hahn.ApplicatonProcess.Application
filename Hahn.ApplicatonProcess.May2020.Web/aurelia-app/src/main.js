73bd2461b2c8e09e37f06a7af3ec1bdab24f3875
azad_aplectrum <azad@aplectrum.com>
2020-05-28 14:50:39 +0600

    Dependency installed

---
+++ Hahn.ApplicatonProcess.May2020.Web/aurelia-app/src/main.js
@@ -0,0 +1,5 @@
+import Aurelia from 'aurelia';
+import { MyApp } from './my-app';
+Aurelia
+    .app(MyApp)
+    .start();
---
+++ Hahn.ApplicatonProcess.May2020.Web/aurelia-app/src/my-app.js
@@ -0,0 +1,5 @@
+export class MyApp {
+    constructor() {
+        this.message = 'Hello World!';
+    }
+}

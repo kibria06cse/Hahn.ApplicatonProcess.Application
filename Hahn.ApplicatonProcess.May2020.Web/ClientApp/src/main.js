591f416102fa499a9e0580830fd0667ed9b8a63c
azad_aplectrum <azad@aplectrum.com>
2020-05-29 21:20:35 +0600

    transalations added

--- Hahn.ApplicatonProcess.May2020.Web/ClientApp/src/main.js
+++ Hahn.ApplicatonProcess.May2020.Web/ClientApp/src/main.js
@@ -1,5 +1,44 @@
 import Aurelia from 'aurelia';
 import { MyApp } from './my-app';
+import { Backend, TCustomAttribute } from 'aurelia-i18n';
 Aurelia
     .app(MyApp)
     .start();
+export function configure(aurelia) {
+    aurelia.use
+        .standardConfiguration()
+        .developmentLogging()
+        .plugin('aurelia-i18n', (instance) => {
+        let aliases = ['t', 'i18n'];
+        // add aliases for 't' attribute
+        TCustomAttribute.configureAliases(aliases);
+        // register backend plugin
+        instance.i18next.use(Backend.with(aurelia.loader));
+        // adapt options to your needs (see http://i18next.com/docs/options/)
+        // make sure to return the promise of the setup method, in order to guarantee proper loading
+        //return instance.setup({
+        //    backend: {                                  // <-- configure backend settings
+        //        loadPath: './locales/{{lng}}/{{ns}}.json', // <-- XHR settings for where to get the files from
+        //    },
+        //    attributes: aliases,
+        //    lng: 'de',
+        //    fallbackLng: 'en',
+        //    debug: false
+        //});
+        return instance.setup({
+            backend: {
+                loadPath: './locales/{{lng}}/{{ns}}.json',
+            },
+            attributes: aliases,
+            lng: 'de-DE',
+            fallbackLng: 'en-us',
+            debug: true,
+            defaultNS: 'translation',
+            ns: [
+                'translation',
+                'other-translations'
+            ]
+        });
+    });
+    aurelia.start().then(a => a.setRoot());
+}
--- Hahn.ApplicatonProcess.May2020.Web/ClientApp/src/my-app.js
+++ Hahn.ApplicatonProcess.May2020.Web/ClientApp/src/my-app.js
@@ -1,5 +1,24 @@
 export class MyApp {
     constructor() {
         this.message = 'Hello World!';
+        this.todos = [];
+        this.heading = 'Todos';
+        this.todos = [];
+        this.todoDescription = '';
+    }
+    addTodo() {
+        if (this.todoDescription) {
+            this.todos.push({
+                description: this.todoDescription,
+                done: false
+            });
+            this.todoDescription = '';
+        }
+    }
+    removeTodo(todo) {
+        let index = this.todos.indexOf(todo);
+        if (index !== -1) {
+            this.todos.splice(index, 1);
+        }
     }
 }

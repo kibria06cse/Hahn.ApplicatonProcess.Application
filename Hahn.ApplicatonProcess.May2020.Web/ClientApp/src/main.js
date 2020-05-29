079d7c5a2d4b954756a4026c92b80b61f4f70340
azad_aplectrum <azad@aplectrum.com>
2020-05-29 23:46:20 +0600

    Aurelia i18n localization added successfully

--- Hahn.ApplicatonProcess.May2020.Web/ClientApp/src/main.js
+++ Hahn.ApplicatonProcess.May2020.Web/ClientApp/src/main.js
@@ -1,57 +1,57 @@
-import Aurelia from 'aurelia';
-import { PLATFORM } from 'aurelia-pal';
-import { MyApp } from './my-app';
-import { Backend, TCustomAttribute } from 'aurelia-i18n';
-Aurelia
-    .app(MyApp)
-    .start();
-export function configure(aurelia) {
-    aurelia.use
-        .standardConfiguration()
-        .developmentLogging()
-        .plugin(PLATFORM.moduleName('aurelia-i18n'), (instance) => {
-        let aliases = ['t', 'i18n'];
-        // add aliases for 't' attribute
-        TCustomAttribute.configureAliases(aliases);
-        // register backend plugin
-        instance.i18next.use(Backend.with(aurelia.loader));
-        // adapt options to your needs (see http://i18next.com/docs/options/)
-        // make sure to return the promise of the setup method, in order to guarantee proper loading
-        return instance.setup({
-            backend: {
-                loadPath: './locales/{{lng}}/{{ns}}.json',
-            },
-            attributes: aliases,
-            lng: 'de-DE',
-            fallbackLng: 'en-us',
-            debug: true,
-            defaultNS: 'translation',
-            ns: [
-                'translation',
-                'other-translations'
-            ]
-        });
-    });
-    //.plugin(PLATFORM.moduleName('aurelia-i18n'), (instance) => {
-    //    let aliases = ['t', 'i18n'];
-    //    // add aliases for 't' attribute
-    //    TCustomAttribute.configureAliases(aliases);
-    //    // register backend plugin
-    //    instance.i18next.use(Backend.with(aurelia.loader));
-    //    return instance.setup({
-    //        backend: {                                  // <-- configure backend settings
-    //            loadPath: './locales/{{lng}}/{{ns}}.json', // <-- XHR settings for where to get the files from
-    //        },
-    //        attributes: aliases,
-    //        lng: 'de-DE',
-    //        fallbackLng: 'en-us',
-    //        debug: true,
-    //        defaultNS: 'translation',
-    //        ns: [
-    //            'translation',
-    //            'other-translations'
-    //        ]
-    //    });
-    //});
-    aurelia.start().then(a => a.setRoot(PLATFORM.moduleName('MyApp')));
-}
+//import Aurelia from 'aurelia';
+//import { PLATFORM } from 'aurelia-pal';
+//import { MyApp } from './my-app';
+//import { Backend, TCustomAttribute } from 'aurelia-i18n';
+//Aurelia
+//    .app(MyApp)
+//    .start();
+//export function configure(aurelia) {
+//    aurelia.use
+//        .standardConfiguration()
+//        .developmentLogging()
+//        .plugin(PLATFORM.moduleName('aurelia-i18n'), (instance) => {
+//        let aliases = ['t', 'i18n'];
+//        // add aliases for 't' attribute
+//        TCustomAttribute.configureAliases(aliases);
+//        // register backend plugin
+//        instance.i18next.use(Backend.with(aurelia.loader));
+//        // adapt options to your needs (see http://i18next.com/docs/options/)
+//        // make sure to return the promise of the setup method, in order to guarantee proper loading
+//        return instance.setup({
+//            backend: {
+//                loadPath: './locales/{{lng}}/{{ns}}.json',
+//            },
+//            attributes: aliases,
+//            lng: 'de-DE',
+//            fallbackLng: 'en-us',
+//            debug: true,
+//            defaultNS: 'translation',
+//            ns: [
+//                'translation',
+//                'other-translations'
+//            ]
+//        });
+//    });
+//    //.plugin(PLATFORM.moduleName('aurelia-i18n'), (instance) => {
+//    //    let aliases = ['t', 'i18n'];
+//    //    // add aliases for 't' attribute
+//    //    TCustomAttribute.configureAliases(aliases);
+//    //    // register backend plugin
+//    //    instance.i18next.use(Backend.with(aurelia.loader));
+//    //    return instance.setup({
+//    //        backend: {                                  // <-- configure backend settings
+//    //            loadPath: './locales/{{lng}}/{{ns}}.json', // <-- XHR settings for where to get the files from
+//    //        },
+//    //        attributes: aliases,
+//    //        lng: 'de-DE',
+//    //        fallbackLng: 'en-us',
+//    //        debug: true,
+//    //        defaultNS: 'translation',
+//    //        ns: [
+//    //            'translation',
+//    //            'other-translations'
+//    //        ]
+//    //    });
+//    //});
+//    aurelia.start().then(a => a.setRoot(PLATFORM.moduleName('MyApp')));
+//}
--- Hahn.ApplicatonProcess.May2020.Web/ClientApp/src/main.ts
+++ Hahn.ApplicatonProcess.May2020.Web/ClientApp/src/main.ts
@@ -1,73 +1,43 @@
-import Aurelia from 'aurelia';
-import { PLATFORM } from 'aurelia-pal';
+import {Aurelia} from 'aurelia-framework'
+import * as environment from '../config/environment.json';
+import {PLATFORM} from 'aurelia-pal';
+import { Backend, TCustomAttribute } from "aurelia-i18n";
+const resBundle = require('i18next-resource-store-loader!./locales/index');
 
-import { MyApp } from './my-app';
-import { I18N, Backend, TCustomAttribute } from 'aurelia-i18n';
+export function configure(aurelia: Aurelia) {
+  aurelia.use
+    .standardConfiguration()
+    .feature(PLATFORM.moduleName('resources/index'));
 
+  aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
 
-Aurelia
-  .app(MyApp)
-    .start();
+  if (environment.testing) {
+    aurelia.use.plugin(PLATFORM.moduleName('aurelia-testing'));
+  }
 
+  aurelia.use.plugin(PLATFORM.moduleName('aurelia-i18n'), (instance) => {
+    let aliases = ['t', 'i18n'];
+    // add aliases for 't' attribute
+    TCustomAttribute.configureAliases(aliases);
 
-export function configure(aurelia) {
-    aurelia.use
-        .standardConfiguration()
-        .developmentLogging()
-        .plugin(PLATFORM.moduleName('aurelia-i18n'), (instance) => {
-        let aliases = ['t', 'i18n'];
-        // add aliases for 't' attribute
-        TCustomAttribute.configureAliases(aliases);
+    // register backend plugin
+    instance.i18next.use(Backend.with(aurelia.loader));
 
-        // register backend plugin
-        instance.i18next.use(Backend.with(aurelia.loader));
-
-        // adapt options to your needs (see http://i18next.com/docs/options/)
-        // make sure to return the promise of the setup method, in order to guarantee proper loading
-        return instance.setup({
-            backend: {                                  // <-- configure backend settings
-                    loadPath: './locales/{{lng}}/{{ns}}.json', // <-- XHR settings for where to get the files from
-                },
-            attributes: aliases,
-            lng: 'de-DE',
-            fallbackLng: 'en-us',
-            debug: true,
-            defaultNS: 'translation',
-            ns: [
-                'translation',
-                'other-translations'
-            ]
-        });
+    // adapt options to your needs (see http://i18next.com/docs/options/)
+    // make sure to return the promise of the setup method, in order to guarantee proper loading
+    return instance.setup({
+      resources: resBundle,
+      attributes: aliases,
+      lng : 'no',
+      fallbackLng : 'en',
+      debug : true,
+      defaultNS: 'translation',
+      ns: [
+        'translation',
+        'other-translations'
+      ]
     });
-        //.plugin(PLATFORM.moduleName('aurelia-i18n'), (instance) => {
-        //    let aliases = ['t', 'i18n'];
-        //    // add aliases for 't' attribute
-        //    TCustomAttribute.configureAliases(aliases);
+  });
 
-        //    // register backend plugin
-        //    instance.i18next.use(Backend.with(aurelia.loader));
-
-
-        //    return instance.setup({
-        //        backend: {                                  // <-- configure backend settings
-        //            loadPath: './locales/{{lng}}/{{ns}}.json', // <-- XHR settings for where to get the files from
-        //        },
-        //        attributes: aliases,
-        //        lng: 'de-DE',
-        //        fallbackLng: 'en-us',
-        //        debug: true,
-        //        defaultNS: 'translation',
-        //        ns: [
-        //            'translation',
-        //            'other-translations'
-        //        ]
-        //    });
-        //});
-
-    aurelia.start().then(a => a.setRoot(PLATFORM.moduleName('MyApp')));
+  aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
 }
-
-
-
-
-
